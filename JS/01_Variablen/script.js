1f9578be31738ec1e8dcc05afce4243041f9156e
Lenifie <leoniesophie.stecher@gmx.at>
2023-10-19 13:48:49 +0200

    JS 01

--- JS/01_Variablen/script.js
+++ JS/01_Variablen/script.js
@@ -10,4 +10,32 @@ console.log("A hat den Wert" + " " +a);
 
 a = 10;
 console.log("A hat den Wert" + " " +a);
-console.log()
+console.log("a + b = " + " " + c);
+
+//Rechnungsarten
+// *, /, + , -
+c = b - a;
+console.log("b - a = " +c);
+
+//Aufgabe
+console.log("Aufgabe");
+
+// Variablen
+a = 7;
+b = 8;
+c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+console.log("a + b + c =" + (a + b + c ));
+console.log("(a+b)/c =" + (a + b) / c);
+console.log("c -a + b =" + (c -a + b));
+console.log("a + b + c * 3 ="+ (a + b + c * 3));
+console.log("(a + b + c)/4 =" +(a + b + c)/4);
+
+// Ändere die Variablen auf folgende Werte
+a = 3
+b = a + 3
+c = c -3
+// gib die neuen Werte der Variablen aus
+console.log("A hat den Wert" + " " +a);
+console.log("B hat den Wert" + " " +b);
+console.log("C hat den Wert" + " " +c);
