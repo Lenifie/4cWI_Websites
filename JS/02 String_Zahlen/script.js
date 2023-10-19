355737e0eb86d16cea40a46db207cd5da3abd0c9
Lenifie <leoniesophie.stecher@gmx.at>
2023-10-19 14:00:36 +0200

    JS 02

---
+++ JS/02 String_Zahlen/script.js
@@ -0,0 +1,36 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+console.log(ab);
+console.log(ab);
+
+// +, -, *, /
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+
+
+
+// Texte STRINGS
+let name = "Andreas";
+console.log(name);
+
+console.log("Das Ergebnis von " + a+ " und " + b + " ist " + ab);
+
+
+//Aufgabe
+console.log("Aufgabe 1")
+let m = 17.4;
+let n = 3.9;
+let result1 = m/n;
+console.log("The result of " + m + " divided by "+ n + " is " + result1)
+
+console.log("Aufgabe 2")
+let firstname = "Leonie-Sophie";
+let lastname = "Stecher";
+console.log("Ich heiße " + firstname +" " + lastname)
