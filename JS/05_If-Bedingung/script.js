34ffa4a6aacf42d3b99a2d6605867c597f2b627d
Lenifie <leoniesophie.stecher@gmx.at>
2023-10-24 08:30:05 +0200

    JS if

---
+++ JS/05_If-Bedingung/script.js
@@ -0,0 +1,30 @@
+let number = 67;
+
+//Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if(number < 20){
+    console.log("Mini")
+}
+else if(number>=20&& number<=50 ){
+    console.log("Medium")
+}
+else if(number>50){
+    console.log("Large")
+}
+
+let number1 = 99;
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if(number1 <=7){
+    console.log("Mini")
+}
+else if(number1>=7&& number1<=90 ){
+    console.log("Medium")
+}
+else if(number1>90){
+    console.log("Large")
+}
