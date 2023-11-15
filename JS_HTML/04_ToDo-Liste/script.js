9b38c5a8f8b3e30d64860b3fe29d1402fedc1ba9
Lenifie <leoniesophie.stecher@gmx.at>
2023-11-15 15:30:00 +0100

    JS_HTML 04 finished new JS_Ajax

--- JS_HTML/04_ToDo-Liste/script.js
+++ JS_HTML/04_ToDo-Liste/script.js
@@ -1,28 +1,48 @@
-let data =[{firstname: "Hans", lastname:"Huber", pointe:30},
-        {firstname: "Paul", lastname:"Müller", pointe:34},
-        {firstname: "Franz", lastname:"Maier", pointe:35}];
-
-//Variante 1
-/*
-for (let index = 0; index < array.length; index++) {
-  const element = data[index];
-  console.log(element.firstname);
-
-}
-
-//Variante 2
-data.forEach(element => {
-  console.log(element.lastname);
-});
-*/
-
-function loadPeople(){
+let todos = [
+  {id: 1, title:"putzen", isDone:false},
+  {id: 2, title:"kochen", isDone:true},
+  {id: 3, title:"einkauf", isDone:false},
+  {id: 4, title:"lernen", isDone:false},
+];
+let maxID= 4;
+printTasks();
+document.getElementById("input").addEventListener("change", function(event){
+  console.log(event.target.value);
+  maxID++;
+  todos.push({id:maxID, title:event.target.value, isDone:false})
+  printTasks();
+  event.target.value = "";
+})
+function printTasks(){
   let html = "";
-  data.forEach(element => {
-    html +=  "<div>" + element.firstname + " " +element.lastname + "</div>"
-    });
-    
-    document.getElementById("content").innerHTML = html;
-    
+  todos.forEach(element => {
+    let style = element.isDone?"green":"none";
+  html += "<div class='task'style='background-color:" + style + "'>" + element.title + " - " + element.isDone + 
+          "<div onClick='setIsDone("+ element.id + 
+          ")'>Done</div><div onClick='deleteTask("+ element.id + ")'>Delete</div></div>"
+  
+  
+  });
+  
+  document.getElementById("todos").innerHTML = html;
 }
-loadPeople();
+
+function deleteTask(id){
+  let index = todos.findIndex(element=>{
+    if(element.id===id) {
+      return true;
+    }
+  })
+  todos.splice(index,1);
+  printTasks();
+  console.log(index);
+}
+function setIsDone(id){
+  let todo = todos.find(element=>{
+      if(element.id===id) {
+        return true;
+      }
+    })
+    todo.isDone = !todo.isDone;
+    printTasks();
+}
